58fdd7c271164742de7139802ee5b302e1225cde
Shashank0606 <shashankrathore606@gmail.com>
2022-11-29 00:37:22 +0530

    REFACTOR: Enter Pre-Define Phone Number

--- regex/src/main/java/com/bridgelabz/regex/Regex.java
+++ regex/src/main/java/com/bridgelabz/regex/Regex.java
@@ -14,19 +14,29 @@ public class Regex {
 		String lastName = scan.nextLine();
 		System.out.println("Enter The Mail : ");
 		String mail = scan.nextLine();
+		System.out.println("Enter The Mobile Number With County Code : ");
+		String mobileNumber = scan.nextLine();
+
 		if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");
 		}
+
 		// Method To Check Last Name With Regex Start With Cap And 3 Min. Character
 		if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");
 		}
+
 		// Method to check Valid Mail with Regex
 		if (Pattern.matches("^[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", mail)) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		if (Pattern.matches("^[+]91{1}\\s[0-9]{10}", mobileNumber)) {
 			System.out.println("True");
 		} else {
 			System.out.println("False");
