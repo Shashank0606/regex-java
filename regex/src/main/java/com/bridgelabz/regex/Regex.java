30bba6db69e796f25ce067226b0e39bd80beb279
Shashank0606 <shashankrathore606@gmail.com>
2022-11-29 00:30:54 +0530

    REFACTOR: Vlid Last Name

--- regex/src/main/java/com/bridgelabz/regex/Regex.java
+++ regex/src/main/java/com/bridgelabz/regex/Regex.java
@@ -7,9 +7,10 @@ public class Regex {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		// Method To Check First Name With Regex Start With Cap And 3 Min. Character
 		System.out.println("Enter the first Name: ");
 		String firstName = scan.nextLine();
+		System.out.println("Enter the last Name :");
+		String lastName = scan.nextLine();
 
 		if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
 			System.out.println("First name is valid");
@@ -17,6 +18,10 @@ public class Regex {
 			System.out.println("Entered first name is invalid");
 		}
 		scan.close();
+		if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
+			System.out.println("Last name is valid");
+		} else {
+			System.out.println("Entered last name is invalid");
+		}
 	}
-
 }
